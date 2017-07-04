5b8f983ba4a7be023c19f6b07e2e5166cd520687
Corentin Treton <corentin.treton@gmail.com>
2017-07-04 16:50:42 +0200

    adding music background

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,9 +2,13 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Silos-Blaster', { preload: preload, create: create, update: update });
 
 function preload() {
+    game.load.audio('bg_music', 'assets/sound/bensound-funnysong.mp3');
 }
 
 function create() {
+    bgMusic = game.add.audio('bg_music');
+    bgMusic.loop = true;
+    bgMusic.play();
 }
 
 function update() {
