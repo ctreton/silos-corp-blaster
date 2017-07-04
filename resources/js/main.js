58cbee39ced53dbb37151b6e0a6a1eaf3bc5ac40
Corentin Treton <corentin.treton@gmail.com>
2017-07-04 14:53:54 +0200

    initial commit

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Silos-Blaster', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+
+}
